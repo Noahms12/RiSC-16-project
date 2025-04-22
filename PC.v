2bcedb8b39c95d44c7bf5d71813a59937e0f1924
HongtaiDu <98723841+HongtaiDu@users.noreply.github.com>
2025-04-22 18:15:24 -0400

    Hongtai: update PC.v

--- PC.v
+++ PC.v
@@ -1,7 +1,8 @@
 module PC (
     input clk,
     input reset,
-    input [1:0] MUXpc_sel,               
+    input [1:0] MUXpc_sel,         
+    input [15:0] alu_result,       
     input [15:0] pc_plus1,        
     input [15:0] pc_plus1_imm,     
     output reg [15:0] pc_out       
@@ -13,7 +14,9 @@ module PC (
         else begin
             case (MUXpc_sel)
                 2'b00: pc_out <= pc_plus1;        
-                2'b01: pc_out <= pc_plus1_imm;       
+                2'b01: pc_out <= pc_plus1_imm;    
+                2'b10: pc_out <= alu_result;      
+                default: pc_out <= pc_plus1;     
             endcase
         end
     end
